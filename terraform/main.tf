47d500a1ae0f3d23086b6d0edc27f2096d00835c
franroad <76252009+franroad@users.noreply.github.com>
2023-06-25 23:06:46 +0200

    Add files via upload
    added vnets and security groups

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,12 +29,57 @@ resource "azurerm_container_registry" "acr" {
     resource_group_name = var.rg_acr
     location = var.location
     sku = "Basic"
+    #add the following option to allow the login with user and pass
     admin_enabled = true
 
   
 }
 
+#create the vnet and the subnets and rg
+
+# we will use the following rg for all the future deployments
+resource "azurerm_resource_group" "rg_compute" {
+  name = var.rg_compute
+  location = var.location
+  
+}
+
+#We will use the following vnet for furutre deployments
+resource "azurerm_virtual_network" "vnet_prac2" {
+  name = var.vnet
+  address_space = ["10.0.0.0/16"]
+  location = var.location
+  resource_group_name = var.rg_compute
+  
+}
+
+#will use the following subnet for further deployments
+resource "azurerm_subnet" "subnet_prac2" {
+  name = var.subnet
+  resource_group_name = var.rg_compute
+  virtual_network_name = var.vnet
+  address_prefixes =["10.0.1.0/24"]
+  
+  
+}
+
+#create a nsg to manage the connectivity of the resources
+resource "azurerm_network_security_group" "prac2_nsg" {
+  name = var.nsg
+  location = var.location
+  resource_group_name = var.rg_compute
+  
+}
 
 
-#Once the resources are defined lets run tf plan
+
+
+
+
+
+
+#Once the resources are defined lets run tf plan and then terraform apply
+
+
+
 
