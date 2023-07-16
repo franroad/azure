dd20a44fcc0b0e1167129f2753e4c581ade8a50d
franroad <76252009+franroad@users.noreply.github.com>
2023-07-16 23:54:35 +0200

    Add files via upload
    updated resources private ip static

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -100,7 +100,7 @@ resource "azurerm_network_interface" "nic_web" {
     ip_configuration {
         name = "nic-web-config"
         subnet_id=azurerm_subnet.subnet_prac2.id
-        private_ip_address_allocation = "Dynamic"
+        private_ip_address_allocation = "Static"
         public_ip_address_id = azurerm_public_ip.publicipweb.id
       
     }  
