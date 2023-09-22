f8e168810b904ed80e2d74f1c524de46a24cf2bc
franroad <76252009+franroad@users.noreply.github.com>
2023-09-22 19:03:22 +0200

    Add files via upload

---
+++ terraform/gcp/resources_info.tf
@@ -0,0 +1,113 @@
+//resource "google_monitoring_dashboard" "dashboard" {
+  //dashboard_json = file("node_dash.json")
+  //project = var.project
+              
+//}
+
+
+//k8s_node - kubernetes.io/node/cpu/allocatable_utilization
+//gcloud beta monitoring channels list --sort-by=name --format="value(name)" 
+
+
+
+
+//WHEN THERE ARE MULTIPLE VALUES:
+
+
+#resource "google_compute_instance" "vm" {
+ 
+#for_each = {
+ 
+#"vm1" = { vm_size = "e2-small", zone = "us-central1-a" }
+ 
+#"vm2" = { vm_size = "e2-medium", zone = "us-central1-b" }
+ 
+#"vm3" = { vm_size = "f1-micro", zone = "us-central1-c" }
+ 
+#}
+ 
+#name = each.key
+ 
+#machine_type = each.value.vm_size
+ 
+#zone = each.value.zone
+ 
+
+ 
+#}
+
+  locals {
+    channels={
+      "a" = "franky1@.com",
+      "b" = "franky2@.com", 
+      "c" = "franky3@.com"
+    }
+  }
+  
+resource "google_monitoring_notification_channel" "noti_channel" {
+  for_each = local.channels
+  display_name = "test -${each.key}"
+  type = "email"
+  labels = {
+    email_address="${each.value}"
+  }
+  
+}
+
+resource "google_monitoring_alert_policy" "allocatable_cpu_node" {
+  display_name = "Allocatable cpu"
+  combiner     = "OR"
+  documentation {
+      content = "testing 1.5 alert"
+  }
+  notification_channels = values(google_monitoring_notification_channel.noti_channel)[*].id
+  conditions {
+    display_name = "node_cpu"
+    condition_threshold {
+      filter     = "metric.type=\"kubernetes.io/node/cpu/allocatable_utilization\" AND resource.type=\"k8s_node\""
+      duration   = "60s"
+      threshold_value = 1.5
+      trigger {
+        count = "1"
+      }
+      comparison = "COMPARISON_GT"
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_MEAN"
+      }
+    
+
+    
+
+      
+      
+    }
+  
+  }
+}
+
+
+
+
+
+
+
+
+output "monitoring_output" {
+ value = google_monitoring_alert_policy.allocatable_cpu_node
+
+}
+
+output "foreach" {
+  value=google_monitoring_notification_channel.noti_channel
+}
+
+
+
+//"name": "${jsonencode(google_monitoring_alert_policy.alerta_policy.id)}"
+
+  
+//locals {
+  //id_alert=google_monitoring_alert_policy.alert_policy.id
+//}
+
